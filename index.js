940bf0db35ecf37cd4a82987c103736ff2f0e0b7
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-16 01:18:31 +0800

    Learned returning values from async function

--- index.js
+++ index.js
@@ -135,9 +135,29 @@ const getDogPic = async () => {
     console.log('Random dog image saved to file!');
   } catch (err) {
     console.log(err);
+    throw err;
   }
+  return '2: READY 🐕';
 };
-getDogPic();
+(async () => {
+  try {
+    console.log('1: Will get dog pics');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('2: Done getting dog pics');
+  } catch (err) {
+    console.log('ERROR 💥');
+  }
+})();
+
+// getDogPic()
+//   .then((x) => {
+//     console.log(x);
+//     console.log('2: Done getting dog pics');
+//   })
+//   .catch((err) => {
+//     console.log('ERROR 💥');
+//   });
 
 // readFilePro(`${__dirname}/dog.txt`)
 //   .then((data) => {
